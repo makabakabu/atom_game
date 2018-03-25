94d73ad8a3103007089e4abbf005eef764fffb8b
vincent <2268317148@qq.com>
2018-03-26 00:45:30 +0800

    commit

--- src/__tests__/sb.test.js
+++
@@ -1,5 +0,0 @@
-import sum from 'sb';
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum({ a: 1, b: 2 })).toBe(3);
-});
--- src/sb.js
+++
@@ -1,5 +0,0 @@
-// @flow
-
-const sum = (props: { a: number, b: number }): number => props.a + props.b;
-
-export default sum;
